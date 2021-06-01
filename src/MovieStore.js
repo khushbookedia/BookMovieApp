ec293cc90649ea36933263e584306bcaf7591704
khushbookedia <khushboo.3990@gmail.com>
2021-06-01 19:10:54 +0530

    Filter functionality added in the Home page for filtering the released movies, based on multiple criterias

--- src/MovieStore.js
+++ src/MovieStore.js
@@ -1,11 +1,12 @@
 import {createStore} from "redux";
 
 
-
+//defining initial state for the store
 const initialState = {
     "movieId": ""
 }
 
+//changing the state as per the action
 function movieReducer (state=initialState, action){
 
     switch (action.type){
